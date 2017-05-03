1983ba59a1c320733fd93187c33ab2e1c0d63c8d
Remco Bloemen <remco-git@xn--2-umb.com>
2017-05-03 19:07:00 +0100

    eslint --fix

--- src/cms.js
+++ src/cms.js
@@ -1,40 +1,38 @@
-import content from "../content.json";
-import {cloneElement} from 'react';
+import content from '../content.json';
+import { cloneElement } from 'react';
 
 const cms = (filename) => {
-    const getString = (string) => {
-        if (content[filename] && content[filename][string]) return content[filename][string];
-        if (content[string]) return content[string];
-        return string;
-    };
-    const traverseJSXTree = (tree) => {
-        if (tree && tree.props && tree.props.children) {
-            if (typeof(tree.props.children) === "string") {
-                return cloneElement(tree, {}, getString(tree.props.children));
-            } else {
-                if (Array.isArray(tree.props.children)) {
-                    let newChildren = tree.props.children.map((child, index)=> {
-                        let node;
-                        if (typeof(child) === "string") {
-                            node = getString(child);
-                        } else {
-                            node = traverseJSXTree(child);
-                            if (node && !node.key){
-                                node = cloneElement(node, {key: index});
-                            }
-                        }
-                        return node;
-                    });
-                    return cloneElement(tree, {}, newChildren);
-                }else{
-                    let newChild = traverseJSXTree(tree.props.children);
-                    return cloneElement(tree, {}, newChild);
-                }
+  const getString = (string) => {
+    if (content[filename] && content[filename][string]) return content[filename][string];
+    if (content[string]) return content[string];
+    return string;
+  };
+  const traverseJSXTree = (tree) => {
+    if (tree && tree.props && tree.props.children) {
+      if (typeof (tree.props.children) === 'string') {
+        return cloneElement(tree, {}, getString(tree.props.children));
+      }
+      if (Array.isArray(tree.props.children)) {
+        const newChildren = tree.props.children.map((child, index) => {
+          let node;
+          if (typeof (child) === 'string') {
+            node = getString(child);
+          } else {
+            node = traverseJSXTree(child);
+            if (node && !node.key) {
+              node = cloneElement(node, { key: index });
             }
-        }
-        return tree;
-    };
-    return traverseJSXTree;
+          }
+          return node;
+        });
+        return cloneElement(tree, {}, newChildren);
+      }
+      const newChild = traverseJSXTree(tree.props.children);
+      return cloneElement(tree, {}, newChild);
+    }
+    return tree;
+  };
+  return traverseJSXTree;
 };
 
 export default cms;
--- src/muiTheme.js
+++ src/muiTheme.js
@@ -1,6 +1,6 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 export default getMuiTheme({
-    palette: {
-        primary1Color: "#d5e20f"
-    }
+  palette: {
+    primary1Color: '#d5e20f',
+  },
 });
